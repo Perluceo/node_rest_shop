9187e7b66f2f16e66a6687a80ab31e2a8185110d
Perluceo <FullStackDevSMU@gmail.com>
2020-01-02 00:46:46 -0600

    CORS headers middleware

--- app.js
+++ app.js
@@ -13,6 +13,21 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+//append headers to overcome CORS errors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-requested, Content-Type, Accept, Authorization");
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    //return next so the other routes can run
+    next();
+});
+
+
 
 //middleware: handles request routes
 app.use('/products', productRoutes);
