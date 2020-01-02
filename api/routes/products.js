2afe2055f8f5832fd15fdbff379ac1a6055c2275
Perluceo <FullStackDevSMU@gmail.com>
2020-01-01 21:42:47 -0600

    patch & delete pass postman

--- api/routes/products.js
+++ api/routes/products.js
@@ -28,5 +28,17 @@ router.get('/:productId', (req, res, next) => {
     }
 });
 
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated product!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted product!'
+    });
+});
+
 
 module.exports = router;
