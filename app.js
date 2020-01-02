3bd0a76eaecff48731bf50390fb27019cb7fc972
Perluceo <FullStackDevSMU@gmail.com>
2020-01-02 00:03:55 -0600

    body-parser json working in postman

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 //logs requests
 app.use(morgan('dev'));
+
+//setting to false selects simple bodies; setting to true selects rich bodies
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 //middleware: handles request routes
 app.use('/products', productRoutes);
